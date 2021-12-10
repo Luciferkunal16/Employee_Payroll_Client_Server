6b9f81018166d2ce821cdbb60f5699c579f3de9a
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-12-10 16:29:40 +0530

    Added UC7:Update Employee Data to server

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 const site_properties = {
-  use_local_storage: "false",
+  use_local_storage: "true",
   home_page: "../pages/home.html",
   add_employee_payroll_page: "../pages/payroll_form.html",
   server_url: "http://127.0.0.1:3000/employee/"
